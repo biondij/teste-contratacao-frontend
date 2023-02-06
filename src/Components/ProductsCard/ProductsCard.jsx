19b723abaefd8e5e621ebe8593c01a92578b8fc1
Biondij <juliabiondi@icloud.com>
2023-02-06 18:30:18 -0300

    remove card links

--- src/Components/ProductsCard/ProductsCard.jsx
+++ src/Components/ProductsCard/ProductsCard.jsx
@@ -72,8 +72,6 @@ function BootstrapCard() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
   );
